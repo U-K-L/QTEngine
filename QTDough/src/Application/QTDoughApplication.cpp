d2d7a67f2a62128e3c171a4de5b4c0e9e4d8e028
U-K-L <WilliamsNoah1720@gmail.com>
2024-01-19 22:30:37 -0500

    Got basic SDL window working

--- QTDough/src/Application/QTDoughApplication.cpp
+++ QTDough/src/Application/QTDoughApplication.cpp
@@ -6,13 +6,49 @@
 
 //extern SDL_Window *SDLWindow;
 int QTDoughApplication::Run() {
+    InitSDLWindow();
 	InitVulkan();
 	return 0;
+}
+
+void QTDoughApplication::InitSDLWindow()
+{
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        //Create window
+        QTSDLWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (QTSDLWindow == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(QTSDLWindow);
+
+            //Fill the surface white
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+            //Update the surface
+            SDL_UpdateWindowSurface(QTSDLWindow);
+
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+        }
+    }
 }
 
 void QTDoughApplication::InitVulkan()
 {
 	CreateInstance();
+    PickPhysicalDevice();
 }
 
 void QTDoughApplication::CreateInstance()
@@ -21,8 +57,9 @@ void QTDoughApplication::CreateInstance()
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
     VkApplicationInfo appInfo{};
     VkInstanceCreateInfo createInfo{};
-
+    
     SDL_Vulkan_GetInstanceExtensions(QTSDLWindow, &extensionCount, NULL); //Get the count.
+    throw std::runtime_error("failed to find a suitable GPU!");
     const char** extensions = (const char**)malloc(sizeof(char*) * extensionCount); //Well justified ;p
     SDL_Vulkan_GetInstanceExtensions(QTSDLWindow, &extensionCount, extensions); //Get the extensions.
 
@@ -39,13 +76,11 @@ void QTDoughApplication::CreateInstance()
     createInfo.ppEnabledExtensionNames = extensions;
     createInfo.enabledLayerCount = 0;
 
-    #ifdef NDEBUG
-        const bool enableValidationLayers = false;
-    #else
-        const bool enableValidationLayers = true;
-    #endif
+    auto sdlExtensions = GetRequiredExtensions();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(sdlExtensions.size());
+    createInfo.ppEnabledExtensionNames = sdlExtensions.data();
 
-    if (enableValidationLayers && !checkValidationLayerSupport()) {
+    if (enableValidationLayers && !CheckValidationLayerSupport()) {
         throw std::runtime_error("validation layers requested, but not available!");
     }
 
@@ -54,6 +89,7 @@ void QTDoughApplication::CreateInstance()
     if (vkCreateInstance(&createInfo, nullptr, &_vkInstance) != VK_SUCCESS) {
         throw std::runtime_error("failed to create instance!");
     }
+
 }
 
 void QTDoughApplication::Cleanup()
@@ -62,7 +98,7 @@ void QTDoughApplication::Cleanup()
     SDL_DestroyWindow(QTSDLWindow);
 }
 
-bool QTDoughApplication::checkValidationLayerSupport() {
+bool QTDoughApplication::CheckValidationLayerSupport() {
     uint32_t layerCount;
     vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
@@ -85,4 +121,71 @@ bool QTDoughApplication::checkValidationLayerSupport() {
     }
 
     return true;
+}
+
+void QTDoughApplication::PickPhysicalDevice()
+{
+    
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(_vkInstance, &deviceCount, nullptr);
+
+    if (deviceCount == 0) {
+        throw std::runtime_error("failed to find GPUs with Vulkan support!");
+    }
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(_vkInstance, &deviceCount, devices.data());
+
+    for (const auto& device : devices) {
+        if (IsDeviceSuitable(device)) {
+            _physicalDevice = device;
+            break;
+        }
+    }
+
+    if (_physicalDevice == VK_NULL_HANDLE) {
+        if (VK_KHR_ray_tracing_pipeline == false)
+            throw std::runtime_error("You need a GPU that supports accelerated raytracing");
+        throw std::runtime_error("failed to find a suitable GPU!");
+    }
+}
+
+bool QTDoughApplication::IsDeviceSuitable(VkPhysicalDevice device) {
+    return false;
+    VkPhysicalDeviceProperties deviceProperties;
+    VkPhysicalDeviceFeatures deviceFeatures;
+    vkGetPhysicalDeviceProperties(device, &deviceProperties);
+    vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+    VkPhysicalDeviceRayTracingPipelineFeaturesKHR rayTracingFeatures = {};
+    rayTracingFeatures.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_RAY_TRACING_PIPELINE_FEATURES_KHR;
+
+    VkPhysicalDeviceFeatures2 deviceFeatures2{};
+    deviceFeatures2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
+    deviceFeatures2.pNext = &rayTracingFeatures;
+
+    vkGetPhysicalDeviceFeatures2(device, &deviceFeatures2);
+
+    return deviceProperties.deviceType ==
+        VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
+        deviceFeatures.geometryShader &&
+        rayTracingFeatures.rayTracingPipeline && false;
+
+    return true;
+}
+
+std::vector<const char*> QTDoughApplication::GetRequiredExtensions() {
+    uint32_t extensionCount = 0;
+
+    SDL_Vulkan_GetInstanceExtensions(QTSDLWindow, &extensionCount, NULL);
+    const char** sdlExtensions = (const char**)malloc(sizeof(char*) * extensionCount);
+    SDL_Vulkan_GetInstanceExtensions(QTSDLWindow, &extensionCount, sdlExtensions);
+
+    std::vector<const char*> extensions(sdlExtensions, sdlExtensions + extensionCount);
+
+    if (enableValidationLayers) {
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    return extensions;
 }
--- QTDough/src/Application/QTDoughApplication.h
+++ QTDough/src/Application/QTDoughApplication.h
@@ -1,4 +1,5 @@
 #pragma once
+#define VK_USE_PLATFORM_WIN32_KHR
 #include <vulkan/vulkan.h>
 #include <SDL2/SDL.h>
 #include <vector>
@@ -7,20 +8,33 @@ const std::vector<const char*> validationLayers = {
     "VK_LAYER_KHRONOS_validation"
 };
 
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
 //QTDough Class.
 class QTDoughApplication {
 public:
     int Run();
     SDL_Window* QTSDLWindow;
+    int SCREEN_WIDTH = 640;
+    int SCREEN_HEIGHT = 520;
 
 
 private:
     //Methods.
+    void InitSDLWindow();
     void InitVulkan();
     void MainLoop();
     void Cleanup();
     void CreateInstance();
-    bool checkValidationLayerSupport();
+    bool CheckValidationLayerSupport();
+    void PickPhysicalDevice();
+    bool IsDeviceSuitable(VkPhysicalDevice device);
+    std::vector<const char*> GetRequiredExtensions();
     //Fields.
     VkInstance _vkInstance;
+    VkPhysicalDevice _physicalDevice = VK_NULL_HANDLE;
 };
