f8f926001bc4fa128e780243b0c4cb7367a28703
UKL <WilliamsNoah1720@gmail.com>
2025-01-06 23:57:39 -0500

    Proper vulkan version

--- QTDoughEngine/src/Engine/RenderPasses/RenderPassObject.h
+++ QTDoughEngine/src/Engine/RenderPasses/RenderPassObject.h
@@ -13,10 +13,10 @@ class RenderPassObject
         ~RenderPassObject();
 
         struct UniformBufferObject {
-            alignas(16) glm::mat4 model;
-            alignas(16) glm::mat4 view;
-            alignas(16) glm::mat4 proj;
-            alignas(16) glm::vec2 texelSize;
+            glm::mat4 model;
+            glm::mat4 view;
+            glm::mat4 proj;
+            float time;
         };
 
         std::vector<UnigmaRenderingObject*> renderingObjects;
